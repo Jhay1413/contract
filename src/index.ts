fcaea071435dbc82c6ba2754a4d054f991642642
Andre <jaguardian00@gmail.com>
2024-10-21 13:47:51 +0800

    Exported ticketingContract

--- src/index.ts
+++ src/index.ts
@@ -401,7 +401,7 @@ const contracts = contract.router({
   userAccounts: userAccountsContract,
   awsContract: awsContract,
   dashboardContract: dashboardContract,
-  ticketingContract: ticketContract,
+  ticketing: ticketContract,
 });
 export {
   companyFormData,
